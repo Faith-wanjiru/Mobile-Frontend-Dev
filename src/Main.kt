92c695005a1b13cf8b1604227ab057277b5004f7
Faith-Wanjiru <wanjirufaithwanyoike@gmail.com>
2024-02-08 23:38:49 +0300

    added a file

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*You are creating an app to capture student records. Some of the data you will
+capture includes full name, age, phone number, weight in kg and citizen. For the
+citizen field you will track whether a student is a Kenyan or not.
+Create variables and assign them some values for each of these fields.
+This assignment will be graded out of 10 points. Submit a github link.*/
+fun main() {
+  var student: String = "Faith"
+  val age: Int = 20;
+  val phonenumber: Int = 751336783
+  val weight: Int = 67;
+  val citizen: Boolean = true
+println("student")
+println("age")
+println("phonenumber")
+println("weight")
+println("citizen")
+
+}
